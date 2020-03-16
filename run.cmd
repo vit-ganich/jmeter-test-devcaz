3bc304f7e73b8ebd3f0af69f0bcc78796430cfa3
Vitali Hanich <vitali.hanich@gmail,com>
2020-03-16 20:07:36 +0300

    updated

--- run.cmd
+++ run.cmd
@@ -1,2 +1,13 @@
-jmeter -n --testfile SlotegratorTestTask.jmx -l Reports/output.csv -e -o Reports
-rem jmeter -n --testfile SlotegratorTestTask.jmx -Jthreads=10 -l Reports/output.csv -e -o Reports
+@echo off
+:: Set datetime format for timestamp
+set mydate=%date:/=_%
+set mytime=%time::=_%
+set mytime=%mytime:,=%
+set mytime=%mytime:.=%
+set _timestamp=%mydate: =_%_%mytime%
+
+:: Create folder with timestamp
+mkdir reports\%_timestamp%\dashboard
+
+:: Rum jmeter tests
+jmeter -n --testfile SlotegratorTestTask.jmx -Jthreads=10 -l reports/%_timestamp%/output.csv -e -o reports/%_timestamp%/dashboard
